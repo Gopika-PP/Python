a41c8998750bfe4302e304f06a20028516af693d
Gopika-PP <gopikapattavayal@gmail.com>
2026-07-29 22:58:46 +0530

    Add files via upload

---
+++ github_challenge1.py
@@ -0,0 +1,10 @@
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+print("\nThe result of :-\n")
+print("Addition:",num1+num2)
+print("Subtraction:",num1-num2)
+print("Multiplication:",num1*num2)
+if num2==0:
+    print("Division by zero is not allowed ")
+else:
+    print("Division:",num1/num2)
